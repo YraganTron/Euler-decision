99742c7c7b41831b2ee1f2e44c6763e353b535d8
Arseny Kirichenko <mr.ars156@gmail.com>
2016-11-01 19:14:48 +0300

    Replacement While in for

--- 29.py
+++ 29.py
@@ -1,12 +1,6 @@
-a = 2
-b = 2
-
 answer = set()
 
-while a <= 100:
-    while b <= 100:
-        answer.add(a**b)
-        b += 1
-    a += 1
-    b = 2
+for x in range(2, 101):
+    for y in range(2, 101):
+        answer.add(x**y)
 print(len(answer))
